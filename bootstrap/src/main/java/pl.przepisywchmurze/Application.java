3b809bf3656f182e0e523956da42e4cb6da398bf
Krzysztof <krzychuuweb@gmail.com>
2025-07-23 13:59:36 +0200

    refactored project to project with hexagonal architecture

--- bootstrap/src/main/java/pl.przepisywchmurze/Application.java
+++ bootstrap/src/main/java/pl.przepisywchmurze/Application.java
@@ -1,13 +1,13 @@
-package pl.przepisywchmurze.api;
+package pl.przepisywchmurze;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(pl.przepisywchmurze.Application.class, args);
 	}
 
 }
